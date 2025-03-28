bc8c7f43489ca9785b38b1ac80ee9ec705d6c7c3
lukasnys <nyslukas@gmail.com>
2025-03-29 00:28:05 +0100

    feat: add gender brand and name

--- scraper/src/product.ts
+++ scraper/src/product.ts
@@ -1,3 +1,5 @@
+import CLIMBING_SHOES = require("./constants/climbing-shoes");
+
 class Product {
   url: string;
   image: string;
@@ -25,16 +27,56 @@ class Product {
     this.isMobileDiscount = isMobileDiscount;
   }
 
-  get brand(): string {
-    return "";
+  get brand() {
+    const brands = Object.values(CLIMBING_SHOES.BRANDS);
+
+    return (
+      brands.find((brand) =>
+        this.scrapedName.toLowerCase().includes(brand.toLowerCase())
+      ) ?? ""
+    );
   }
 
   get name(): string {
-    return "";
+    const brand = this.brand;
+    if (!brand) return "";
+
+    const shoes =
+      CLIMBING_SHOES.SHOES[brand as keyof typeof CLIMBING_SHOES.SHOES];
+
+    // Try to find an exact match first
+    return (
+      shoes.find((shoe) =>
+        this.scrapedName.toLocaleLowerCase().includes(shoe.toLowerCase())
+      ) ?? ""
+    );
   }
 
-  get gender(): "M" | "F" | "" {
-    return "";
+  get gender(): "M" | "F" | "U" {
+    const WOMEN_KEYWORDS = [
+      "women",
+      "womens",
+      "women's",
+      "female",
+      "wmn",
+      "w's",
+    ];
+    const MEN_KEYWORDS = ["men", "mens", "men's", "male", "m's"];
+
+    if (
+      WOMEN_KEYWORDS.some((keyword) =>
+        this.scrapedName.toLowerCase().includes(keyword)
+      )
+    ) {
+      return "F";
+    } else if (
+      MEN_KEYWORDS.some((keyword) =>
+        this.scrapedName.toLowerCase().includes(keyword)
+      )
+    ) {
+      return "M";
+    }
+    return "U";
   }
 }
 
