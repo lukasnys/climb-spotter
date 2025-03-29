47286a4e3fccf673a9be6fd64b37e1cb94bc26c3
lukasnys <nyslukas@gmail.com>
2025-03-29 01:08:43 +0100

    cleanup: split up into functions

--- scraper/src/scrapers/oliunid.ts
+++ scraper/src/scrapers/oliunid.ts
@@ -2,72 +2,98 @@ import puppeteer = require("puppeteer");
 import Store = require("../store");
 import Product = require("../product");
 
-async function scrapeOliunid(): Promise<Store> {
-  const BASE_URL = "https://www.oliunid.com/eu/footwear/climbing-shoes";
+const BASE_URL = "https://www.oliunid.com/eu/footwear/climbing-shoes";
 
+interface ScrapedProductData {
+  url: string | undefined | null;
+  name: string | undefined | null;
+  image: string | undefined | null;
+  originalPrice: string | undefined | null;
+  discountPrice: string | undefined | null;
+  discount: number | undefined | null;
+}
+
+async function scrapeOliunid(): Promise<Store> {
   const browser = await puppeteer.launch({
     headless: true,
     defaultViewport: { width: 1920, height: 1080 },
   });
 
-  const page = await browser.newPage();
-  await page.goto(BASE_URL, { waitUntil: "networkidle2" });
+  try {
+    const page = await browser.newPage();
+    const allProductData = await scrapeAllPages(page);
+    const products = convertToProducts(allProductData);
 
-  const productData = [];
+    return new Store("Oliunid", "EUR", "https://www.oliunid.com/", products);
+  } finally {
+    await browser.close();
+  }
+}
+
+async function scrapeAllPages(page: puppeteer.Page) {
+  const allProductData: ScrapedProductData[] = [];
 
   let currentPage = 1;
   let hasNextPage = true;
+
   while (hasNextPage) {
     const url = `${BASE_URL}?p=${currentPage}`;
     await page.goto(url, { waitUntil: "networkidle2" });
-    console.log("Scraping page:", url);
 
-    const pageProductData = await page.evaluate(() => {
-      const elements = document.querySelectorAll(".product-item");
-      return Array.from(elements)
-        .filter((element) => !!element.querySelector(".original-price-wrapper"))
-        .map((element) => {
-          const url = element
-            .querySelector(".product-item-link")
-            ?.getAttribute("href");
-          const name = element
-            .querySelector(".product-item-link")
-            ?.textContent?.trim();
-          const image = element.querySelector("img")?.getAttribute("data-src");
+    const pageProductData = await scrapeProductsFromPage(page);
+    allProductData.push(...pageProductData);
 
-          const originalPrice = element
-            .querySelector(".original-price-wrapper [data-price-amount]")
-            ?.getAttribute("data-price-amount");
-          const discountPrice = element
-            .querySelector(".normal-price [data-price-amount]")
-            ?.getAttribute("data-price-amount");
-
-          const discountString = element.querySelector(
-            ".discount-percentage.desktop-only"
-          )?.textContent;
-          const discount = discountString
-            ? parseFloat(discountString)
-            : undefined;
-
-          return { url, name, image, originalPrice, discountPrice, discount };
-        });
-    });
-
-    productData.push(...pageProductData);
-
-    hasNextPage = await page.evaluate(() => {
-      const nextPageButton = document.querySelector(".action.next");
-      return !!nextPageButton;
-    });
-
-    if (hasNextPage) {
-      currentPage++;
-    }
+    hasNextPage = await hasNextPageAvailable(page);
+    currentPage++;
   }
 
-  await browser.close();
+  return allProductData;
+}
 
-  const products = productData
+async function hasNextPageAvailable(page: puppeteer.Page): Promise<boolean> {
+  return await page.evaluate(() => {
+    const nextPageButton = document.querySelector(".action.next");
+    return !!nextPageButton;
+  });
+}
+
+async function scrapeProductsFromPage(
+  page: puppeteer.Page
+): Promise<ScrapedProductData[]> {
+  return page.evaluate(() => {
+    const elements = document.querySelectorAll(".product-item");
+    return Array.from(elements)
+      .filter((element) => !!element.querySelector(".original-price-wrapper"))
+      .map((element) => {
+        const url = element
+          .querySelector(".product-item-link")
+          ?.getAttribute("href");
+        const name = element
+          .querySelector(".product-item-link")
+          ?.textContent?.trim();
+        const image = element.querySelector("img")?.getAttribute("data-src");
+
+        const originalPrice = element
+          .querySelector(".original-price-wrapper [data-price-amount]")
+          ?.getAttribute("data-price-amount");
+        const discountPrice = element
+          .querySelector(".normal-price [data-price-amount]")
+          ?.getAttribute("data-price-amount");
+
+        const discountString = element.querySelector(
+          ".discount-percentage.desktop-only"
+        )?.textContent;
+        const discount = discountString
+          ? parseFloat(discountString)
+          : undefined;
+
+        return { url, name, image, originalPrice, discountPrice, discount };
+      });
+  });
+}
+
+function convertToProducts(productData: ScrapedProductData[]): Product[] {
+  return productData
     .map((product) =>
       Product.create(
         product.url,
@@ -79,15 +105,6 @@ async function scrapeOliunid(): Promise<Store> {
       )
     )
     .filter((product) => product !== null);
-
-  const store = new Store(
-    "Oliunid",
-    "EUR",
-    "https://www.oliunid.com/",
-    products
-  );
-
-  return store;
 }
 
 export = scrapeOliunid;
