f3bc5f43cd131d9f769f8cff0e18b069f6a5ad31
lukasnys <nyslukas@gmail.com>
2025-04-05 13:58:51 +0200

    add proxy in dev mode

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -2,14 +2,29 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-export default defineConfig({
-  plugins: [
-    classicEmberSupport(),
-    ember(),
-    // extra plugins here
-    babel({
-      babelHelpers: 'runtime',
-      extensions,
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [
+      classicEmberSupport(),
+      ember(),
+      // extra plugins here
+      babel({
+        babelHelpers: 'runtime',
+        extensions,
+      }),
+    ],
+  };
+
+  if (mode === 'development') {
+    config.server = {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8788',
+          changeOrigin: true,
+        },
+      },
+    };
+  }
+
+  return config;
 });
