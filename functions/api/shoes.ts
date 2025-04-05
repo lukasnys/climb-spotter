48910c08b6598f7e29bef6a51976fb86dac85d52
lukasnys <nyslukas@gmail.com>
2025-04-05 15:05:38 +0200

    testt

--- functions/api/shoes.ts
+++ functions/api/shoes.ts
@@ -1,7 +1,16 @@
 import { GoogleSheets, Shoe } from "@climbing-deals/shared";
 
 export async function onRequest(context) {
-  const googleSheets = GoogleSheets.createWithEnv("shoes");
+  const GOOGLE_SPREADSHEET_ID = context.env.GOOGLE_SPREADSHEET_ID;
+  const GOOGLE_PRIVATE_KEY = context.env.GOOGLE_PRIVATE_KEY;
+  const GOOGLE_CLIENT_EMAIL = context.env.GOOGLE_CLIENT_EMAIL;
+
+  const googleSheets = new GoogleSheets(
+    "shoes",
+    GOOGLE_SPREADSHEET_ID,
+    GOOGLE_PRIVATE_KEY,
+    GOOGLE_CLIENT_EMAIL
+  );
   const rows = await googleSheets.getRows<Shoe>();
 
   const shoes = rows.map((row) => {
