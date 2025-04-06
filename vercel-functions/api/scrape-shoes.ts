0983788bf4e7feeceb4dd59f006f28909834042a
lukasnys <nyslukas@gmail.com>
2025-04-06 12:04:14 +0200

    setup redis

--- vercel-functions/api/scrape-shoes.ts
+++ vercel-functions/api/scrape-shoes.ts
@@ -1,4 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { createClient } from "redis";
 
 import { scrapeShoeDeals } from "@climbing-deals/scraper";
 
@@ -6,6 +7,12 @@ export default async function handler(
   request: VercelRequest,
   response: VercelResponse
 ) {
+  const redis = await createClient({ url: process.env.REDIS_URL }).connect();
   const shoes = await scrapeShoeDeals();
+
+  await redis.set("shoes", JSON.stringify(shoes), {
+    EX: 60 * 60 * 24, // 1 day
+  });
+
   response.status(200).json(shoes);
 }
