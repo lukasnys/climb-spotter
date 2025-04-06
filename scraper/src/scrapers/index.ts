196d07b6c457e04264991f638c1869e9024a75df
lukasnys <nyslukas@gmail.com>
2025-04-06 12:06:30 +0200

    fix?

--- scraper/src/scrapers/index.ts
+++ scraper/src/scrapers/index.ts
@@ -3,6 +3,7 @@ import { logger, RetailerKey, RETAILERS } from "@climbing-deals/shared";
 import { Shoe } from "../Shoe.js";
 import { type Shoe as ShoeData } from "@climbing-deals/shared";
 import { z } from "zod";
+import chromium from "@sparticuz/chromium";
 
 declare global {
   interface Element {
@@ -99,8 +100,10 @@ export abstract class Scraper {
     logger.info(`Scraping ${retailerInfo.name}...`);
 
     const browser = await puppeteer.launch({
-      headless: true,
-      defaultViewport: { width: 1280, height: 800 },
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
     });
 
     try {
